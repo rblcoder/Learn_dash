f134ee668dd92cd244a6f256fb1c282de541f568
rblcoder <35212670+rblcoder@users.noreply.github.com>
2019-10-23 12:03:17 +0530

    Added option to choose different datasets

--- learn_dash_3.py
+++ learn_dash_3.py
@@ -8,21 +8,48 @@ import plotly.graph_objs as go
 from sklearn import datasets
 
 iris = datasets.load_iris()
-df = pd.DataFrame(iris.data, columns=iris.feature_names)
-df['target'] = iris.target
+breast_cancer = datasets.load_breast_cancer()
+dataset_chosen = 'iris'
+def load_data(dataset):
+    if dataset == 'iris':
+        df = pd.DataFrame(iris.data, columns=iris.feature_names)
+        df['target'] = iris.target
+        targets = list(df['target'].unique())
+        target_names = iris.target_names
+    else:
+        df = pd.DataFrame(breast_cancer.data, columns=breast_cancer.feature_names)
+        df['target'] = breast_cancer.target
+        targets = list(df['target'].unique())
+        target_names = breast_cancer.target_names
+    return df, targets, target_names
 
+df, targets, target_names = load_data(dataset_chosen)
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div([
     html.Div([
+        html.Div(
+            [
+                html.H5("Scatter Plots"),
+                dcc.Dropdown(
+                id='choose-dataset',
+                options=[{'label': i, 'value': i} for i in ['iris', 'breast cancer']],
+                value='iris'
+            ),
+                html.P('Select columns to be chosen for X axis and Y axis')
+            ],
 
+        ),
         html.Div([
+            html.Div(
+                [html.H6("X: axis")]
+            ),
             dcc.Dropdown(
                 id='xaxis-column',
-                options=[{'label': i, 'value': i} for i in df.columns],
-                value='sepal length (cm)'
+                options=[{'label': i, 'value': i} for i in df.columns[:-1]],
+                value=df.columns[0]
             ),
             dcc.RadioItems(
                 id='xaxis-type',
@@ -31,13 +58,16 @@ app.layout = html.Div([
                 labelStyle={'display': 'inline-block'}
             )
         ],
-        style={'width': '48%', 'display': 'inline-block'}),
+            style={'width': '48%', 'display': 'inline-block'}),
 
         html.Div([
+            html.Div(
+                [html.H6("Y: axis")]
+            ),
             dcc.Dropdown(
                 id='yaxis-column',
-                options=[{'label': i, 'value': i} for i in df.columns],
-                value='sepal width (cm)'
+                options=[{'label': i, 'value': i} for i in df.columns[:-1]],
+                value=df.columns[1]
             ),
             dcc.RadioItems(
                 id='yaxis-type',
@@ -45,13 +75,42 @@ app.layout = html.Div([
                 value='Linear',
                 labelStyle={'display': 'inline-block'}
             )
-        ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+        ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
     ]),
 
     dcc.Graph(id='indicator-graphic'),
 
-
 ])
+
+@app.callback(
+    Output('xaxis-column', 'options'),
+    [Input('choose-dataset', 'value')])
+def set_xaxis_column_options(the_dataset):
+    global dataset_chosen, df, targets, target_names
+    if dataset_chosen != the_dataset:
+        dataset_chosen = the_dataset
+        df, targets, target_names = load_data(dataset_chosen)
+    return [{'label': i, 'value': i} for i in df.columns[:-1]]
+
+
+@app.callback(
+    Output('xaxis-column', 'value'),
+    [Input('xaxis-column', 'options')])
+def set_xaxis_column_value(available_options):
+    return available_options[0]['value']
+
+@app.callback(
+    Output('yaxis-column', 'options'),
+    [Input('xaxis-column', 'value')])
+def set_xaxis_column_options(the_dataset):
+    return [{'label': i, 'value': i} for i in df.columns[:-1]]
+
+
+@app.callback(
+    Output('yaxis-column', 'value'),
+    [Input('yaxis-column', 'options')])
+def set_yaxis_column_value(available_options):
+    return available_options[1]['value']
 
 @app.callback(
     Output('indicator-graphic', 'figure'),
@@ -59,25 +118,25 @@ app.layout = html.Div([
      Input('yaxis-column', 'value'),
      Input('xaxis-type', 'value'),
      Input('yaxis-type', 'value'),
-
+     Input('choose-dataset', 'value'),
      ])
 def update_graph(xaxis_column_name, yaxis_column_name,
                  xaxis_type, yaxis_type,
-
+                 choose_dataset,
                  ):
-
 
     return {
         'data': [go.Scatter(
-            x=df[xaxis_column_name],
-            y=df[yaxis_column_name],
+            x=df.loc[df['target']==target, xaxis_column_name],
+            y=df.loc[df['target']==target, yaxis_column_name],
             mode='markers',
+            name=target_names[target],
             marker={
                 'size': 15,
                 'opacity': 0.5,
                 'line': {'width': 0.5, 'color': 'white'}
             }
-        )],
+        ) for target in targets],
         'layout': go.Layout(
             xaxis={
                 'title': xaxis_column_name,
